a515fda163e0f300c96722254764060f650162d2
darksoldier1404 <darksoldier1404@gmail.com>
2022-02-16 18:34:03 +0900

    fix MoneyAPI#ess is always null

--- V1_14_4/src/main/java/com/darksoldier1404/dppc/DPPCore.java
+++ V1_14_4/src/main/java/com/darksoldier1404/dppc/DPPCore.java
@@ -46,6 +46,8 @@ public class DPPCore extends JavaPlugin {
             getLogger().warning("Essentials 플러그인이 설치되어있지 않습니다.");
             getLogger().warning("MoneyAPI 사용 불가.");
             return;
+        }else{
+            ess = (Essentials) pl;
         }
         ess = (Essentials) pl;
         Bukkit.getScheduler().runTaskLater(plugin, () -> enabledPlugins.keySet().forEach(SchedulerUtils::initUpdateChecker), 1200L);
